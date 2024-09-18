cc3fffb4c7232ce83cd2887e00c75fa0d1831970
Esra-Demirtas <demirtassesraa@gmail.com>
2024-09-18 15:45:32 +0300

    Ürün listesi veri tabanında getirildi ve dinamikleştirildi. Product_model kodlandı.

---
+++ panel/application/models/Product_model.php
@@ -0,0 +1,14 @@
+<?php
+class Product_model extends CI_Model
+{
+    public $tableName = "products";
+    public function product_list(){
+        parent::__construct();
+    }
+
+    /**Tüm kayıtları bana getirecek olan metod**/
+    public function get_all(){
+
+        return $this->db->get($this->tableName)->result();
+    }
+}
