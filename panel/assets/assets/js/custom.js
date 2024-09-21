7e790148f16730cc638e50ab0e9e14e33dc014bc
Esra-Demirtas <demirtassesraa@gmail.com>
2024-09-21 18:48:22 +0300

    sweetAlert ile confirm kullanılarak kayıt silme işlemi gerçekleştirildi.

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,19 +1,28 @@
 $(document).ready(function (){
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                title: "Deleted!",
-                text: "Your file has been deleted.",
-                icon: "success"
-            });
-        }
-    });
+    $(".remove-btn").click(function (e){
+
+        var $data_url = $(this).data("url");
+
+        Swal.fire({
+            title: "Kaydı silmek istediğinizden emin misiniz?",
+            text: "Bu işlemi geri alamayacaksınız!",
+            icon: "uyarı",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Evet, Sil!",
+            cancelButtonText: "Hayır"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                window.location.href = $data_url;
+
+                /*Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });*/
+            }
+        });
+    })
 })
