76c109ddbf803b230fe76b95c51df94ede300242
Esra-Demirtas <demirtassesraa@gmail.com>
2024-09-21 16:51:01 +0300

    Ürün update işlemi gerçekleştirildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -100,4 +100,58 @@ class Product extends CI_Controller
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
 
     }
+
+    public function update($id){
+
+        $this->load->library("form_validation");
+        //Kurallar yazılır. Daha sonra form validation çalıştırılır. Başarılı ise kayıt işlemi başlar başarısız ise hata mesajı sayfada görünür.
+
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                //required'a ait bir hata meydana geldiğinde ilgili mesajı verecek.
+                "required" => "{field} alanını doldurulmalıdır."
+            )
+        );
+
+        $validate = $this->form_validation->run();
+
+        if($validate){
+            $update = $this->product_model->update(
+                array(
+                    "id" => $id
+                ),
+                array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSEO($this->input->post("title")),
+                )
+            );
+
+            //TODO Alert Sistemi Eklenecek
+            if($update){
+                redirect(base_url("product"));
+            }else{
+                redirect(base_url("product"));
+            }
+
+        }else{
+            $viewData = new stdClass();
+
+            /**Tablodan verilerin getirilmesi**/
+            $item = $this->product_model->get(
+                array(
+                    "id" => $id
+                )
+            );
+
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder =  "update";
+            $viewData->form_error = true;
+            $viewData->item = $item;
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+    }
 }
