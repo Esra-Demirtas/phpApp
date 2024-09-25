4a086269cc58620ab863f8948eebd29e740eeed1
Esra-Demirtas <demirtassesraa@gmail.com>
2024-09-25 11:28:23 +0300

    Ürün resimlerinin silinmesi işlemi yapıldı.

--- panel/application/helpers/tools_helper.php
+++ panel/application/helpers/tools_helper.php
@@ -7,3 +7,28 @@ function convertToSEO($text){
 
     return strtolower(str_replace($turkce, $convert, $text));
 }
+
+/*function getFileName($id){
+    $fileName = $this->product_image_model->get(
+        array(
+            "id"    => $id
+        )
+    );
+    return $fileName->img_url;
+}*/
+
+/*function getFileName($id) {
+    // CodeIgniter'ın global instance'ına eriş
+    $ci =& get_instance();
+
+    // Modeli yükle
+    $ci->load->model('product_image_model');
+
+    // Veritabanından dosya adını al
+    $fileName = $ci->product_image_model->get(array(
+        "id" => $id
+    ));
+
+    // Eğer dosya adı mevcutsa img_url döndür
+    return $fileName ? $fileName->img_url : null;
+}*/
--- panel/application/views/product_v/image/render_elements/image_list_v.php
+++ panel/application/views/product_v/image/render_elements/image_list_v.php
@@ -48,7 +48,7 @@
                     />
                 </td>
                 <td class="w100 text-center">
-                    <button data-url="<?php echo base_url("product/delete"); ?>" type="button" class="btn btn-sm btn-block btn-danger btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</button>
+                    <button data-url="<?php echo base_url("product/imageDelete/$image->id/$image->product_id"); ?>" type="button" class="btn btn-sm btn-block btn-danger btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</button>
                 </td>
             </tr>
 
