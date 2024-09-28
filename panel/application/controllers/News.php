0f57a28605a128d59099278bc7f1cfbafdd041da
Esra-Demirtas <demirtassesraa@gmail.com>
2024-09-28 14:04:36 +0300

    Güncelleme işlemi için view tasarımı yapıldı.

--- panel/application/controllers/News.php
+++ panel/application/controllers/News.php
@@ -298,31 +298,6 @@ class News extends CI_Controller
         redirect(base_url("news"));
     }
 
-    public function imageDelete($id, $parent_id)
-    {
-        $fileName = $this->news_image_model->get(
-            array(
-                "id"    => $id
-            )
-        );
-
-        $delete = $this->news_image_model->delete(
-            array(
-                "id"    => $id
-            )
-        );
-
-        //TODO Alert Sistemi Eklenecek
-        if($delete){
-
-            unlink("uploads/{$this->viewFolder}/$fileName->img_url");
-
-            redirect(base_url("news/image_form/$parent_id"));
-        } else {
-            redirect(base_url("news/image_form/$parent_id"));
-        }
-    }
-
     public function isActiveSetter($id)
     {
         if ($id){
@@ -337,66 +312,6 @@ class News extends CI_Controller
                 )
             );
         }
-    }
-
-    public function imageIsActiveSetter($id)
-    {
-        if ($id){
-            $isActive = ($this->input->post("data") === "true") ? 1 : 0;
-
-            $this->news_image_model->update(
-                array(
-                    "id" => $id
-                ),
-                array(
-                    "isActive" => $isActive
-                )
-            );
-        }
-    }
-
-    public function isCoverSetter($id, $parent_id)
-    {
-        if ($id && $parent_id){
-            $isCover = ($this->input->post("data") === "true") ? 1 : 0;
-
-            //Kapak olması istenen resim
-            $this->news_image_model->update(
-                array(
-                    "id"            => $id,
-                    "news_id"    => $parent_id
-                ),
-                array(
-                    "isCover"       => $isCover
-                )
-            );
-
-            //Kapak olması istenmeyen diğer resimler
-            $this->news_image_model->update(
-                array(
-                    "id !="         => $id,
-                    "news_id"    => $parent_id
-                ),
-                array(
-                    "isCover"       => 0
-                )
-            );
-        }
-
-        $viewData = new stdClass();
-
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder =  "image";
-
-        $viewData->item_images = $this->news_image_model->get_all(
-            array(
-                "news_id" => $parent_id
-            ), "rank ASC"
-        );
-
-        $render_html = $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/render_elements/image_list_v", $viewData, true);
-
-        echo $render_html;
     }
 
     public function rankSetter()
@@ -422,103 +337,6 @@ class News extends CI_Controller
                 )
             );
         }
-    }
-
-    public function imageRankSetter()
-    {
-
-        $data = $this->input->post("data");
-
-        parse_str($data, $order);
-
-        $items = $order["ord"];
-
-        //print_r($items);
-
-        foreach ($items as $rank => $id){
-
-            $this->news_image_model->update(
-                array(
-                    "id"        => $id,
-                    "rank !="   => $rank
-                ),
-                array(
-                    "rank"      => $rank
-                )
-            );
-        }
-    }
-
-    public function image_form($id)
-    {
-        $viewData = new stdClass();
-
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder =  "image";
-
-        $viewData->item = $this->news_model->get(
-            array(
-                "id" => $id
-            )
-        );
-
-        $viewData->item_images =  $this->news_image_model->get_all(
-            array(
-                "news_id" => $id
-            ), "rank ASC"
-        );
-
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-    }
-
-    public function image_upload($id){
-
-        $file_name = convertToSEO(pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
-
-        $config["allowed_types"] = "gif|jpg|png|jpeg";
-        $config["upload_path"] = "uploads/$this->viewFolder/";
-        $config["file_name"] = $file_name;
-
-        $this->load->library("upload", $config);
-
-        $upload = $this->upload->do_upload("file");
-
-        if($upload){
-
-            $uploaded_file = $this->upload->data("file_name");
-
-            $this->news_image_model->add(
-                array(
-                    "img_url"        => $uploaded_file,
-                    "rank"           => 0,
-                    "isActive"       => 1,
-                    "isCover"        => 0,
-                    "createdAt"      => date("Y-m-d H:i:s"),
-                    "news_id"     => $id
-                )
-            );
-        } else {
-            echo "işlem başarısız";
-        }
-    }
-
-    public function refresh_image_list($id)
-    {
-        $viewData = new stdClass();
-
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder =  "image";
-
-        $viewData->item_images = $this->news_image_model->get_all(
-            array(
-                "news_id" => $id
-            )
-        );
-
-        $render_html = $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/render_elements/image_list_v", $viewData, true);
-
-        echo $render_html;
-
     }
 
 }
